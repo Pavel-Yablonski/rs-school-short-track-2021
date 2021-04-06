cd3b9234ffb14bd0416b2c5de09eb22e4ba56bbc
Pavel Yablonski <yablonski.pavel81@gmail.com>
2021-04-07 01:26:35 +0300

    feat: complete task mine-sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,6 +1,6 @@
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
+ * that don't contain a mine have a count in it that indicates the total count of mines
  * in the neighboring cells. Starting off with some arrangement of mines
  * we want to create a Minesweeper game setup.
  *
@@ -21,8 +21,52 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const arr = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    arr.push([]);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+
+      if (matrix[i - 1] && matrix[i - 1][j - 1]) {
+        count++;
+      }
+
+      if (matrix[i - 1] && matrix[i - 1][j]) {
+        count++;
+      }
+
+      if (matrix[i - 1] && matrix[i - 1][j + 1]) {
+        count++;
+      }
+
+      if (matrix[i][j - 1]) {
+        count++;
+      }
+
+      if (matrix[i][j + 1]) {
+        count++;
+      }
+
+      if (matrix[i + 1] && matrix[i + 1][j - 1]) {
+        count++;
+      }
+
+      if (matrix[i + 1] && matrix[i + 1][j]) {
+        count++;
+      }
+
+      if (matrix[i + 1] && matrix[i + 1][j + 1]) {
+        count++;
+      }
+
+      arr[i].push(count);
+    }
+  }
+
+  return arr;
 }
 
 module.exports = minesweeper;
