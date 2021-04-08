2c2889bc5f2f83aa5261c7e98b3e531b3ecd7544
Pavel <yablonski.pavel81@gmail.com>
2021-04-08 19:49:40 +0300

    feat: complete task queue

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -13,15 +13,39 @@
 
 class Queue {
   get size() {
-    throw new Error('Not implemented');
+    let count = 0;
+    let head = this.queue;
+    while (head) {
+      head = head.next;
+      count++;
+    }
+    return count;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  enqueue(element) {
+    const list = new ListNode(element);
+    list.value = element;
+    if (this.queue) {
+      list.next = this.queue;
+    }
+    this.queue = list;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    let head = this.queue;
+
+    if (this.size === 1) {
+      const element = head.value;
+      this.queue = null;
+      return element;
+    }
+
+    while (head.next.next) head = head.next;
+
+    const element = head.next.value;
+    head.next = null;
+
+    return element;
   }
 }
 
