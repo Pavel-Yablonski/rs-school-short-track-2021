ea4c51a4d357694aef351f92c04c94e61ca84dd5
Pavel <yablonski.pavel81@gmail.com>
2021-04-08 19:24:21 +0300

    feat: complete task dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  if (!domains.length) return {};
+
+  const result = {};
+  let resultItem = '';
+
+  const splitArr = [];
+  const filterArr = [];
+
+  for (let i = 0; i < domains.length; i++) {
+    const splitItem = domains[i].split('.');
+
+    for (let j = splitItem.length - 1; j >= 0; j--) {
+      splitArr.push(splitItem[j]);
+    }
+  }
+
+  const set = new Set(splitArr);
+
+  set.forEach((item) => {
+    filterArr.push(item);
+  });
+
+  for (let i = 0; i < filterArr.length; i++) {
+    resultItem += `.${filterArr[i]}`;
+
+    if (i < 2) result[resultItem] = domains.length;
+    else result[resultItem] = 1;
+  }
+
+  return result;
 }
 
 module.exports = getDNSStats;
